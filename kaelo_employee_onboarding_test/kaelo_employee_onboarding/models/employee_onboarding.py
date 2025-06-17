2631dc5865876cbb8569523495ef5af862a59bf3
nomfundo3 <124263197+nomfundo3@users.noreply.github.com>
2025-06-17 18:13:46 +0200

    Update employee_onboarding.py

--- kaelo_employee_onboarding_test/kaelo_employee_onboarding/models/employee_onboarding.py
+++ kaelo_employee_onboarding_test/kaelo_employee_onboarding/models/employee_onboarding.py
@@ -4,4 +4,8 @@ class EmployeeOnboarding(models.Model):
     _name = 'employee.onboarding'
     _description = 'Employee Onboarding'
 
+    full_name = fields.Char(string='Full Name', required=True)
+    id_number = fields.Char(string='ID Number', required=True)
+    date_of_birth = fields.Date(string='Date of Birth', required=True)
+    employee_number = fields.Char(string='Employee
     # create a view,action and menu for this model as well
